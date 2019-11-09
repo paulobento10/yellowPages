1e8349045643d79a31fb9ebcb788bdccf8e3dd71
JoaoCoelho8 <33787@ufp.edu.pt>
2019-11-09 17:39:02 +0000

    new search field

--- vue/myapp/src/components/ShowUsers.vue
+++ vue/myapp/src/components/ShowUsers.vue
@@ -1,35 +1,55 @@
 <template>
-  <div id="users">
-
-    <div>
-      Name: <input v-model="name" placeholder="123">
-      Local: <input v-model="local" placeholder="123">
-      <button id="btn" class="" v-on:click="getUserNameLocal(name,local)">Search</button>
+  <div>
+    <div style="background:transparent !important" class="jumbotron">
+      <fieldset class="border border-dark">
+        <legend id="Legend5" runat="server" visible="true" style="width:auto; margin-bottom:0px; margin-left:40px; font-size:20px; font-weight:bold;">Inserir Nova Empresa</legend>
+        <b-container fluid style="margin-left:175px;">
+          <b-row class="my-1">
+            <b-col sm="0">
+              <label for="input-small">Nome:</label>
+            </b-col>
+            <b-col sm="4">
+              <b-form-input id="input-small" size="sm" v-model="name" placeholder="Nome da Empresa"></b-form-input>
+            </b-col>
+            <b-col sm="0">
+              <label for="input-small">Morada:</label>
+            </b-col>
+            <b-col sm="4">
+              <b-form-input id="input-small" size="sm" v-model="local" placeholder="Local da Empresa"></b-form-input>
+            </b-col>
+            <b-col sm="3">
+              <b-button variant="outline-primary" v-on:click="getUserNameLocal(name,local); updateCounter(users)">Pesquisar</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </fieldset>
     </div>
+
 
     <div v-for="(user, index) in users" :key="user.id">
       <p>{{user.name}} my phone number: {{user.phoneNumber}} and counter: {{user.counter}}</p>
     </div>
 
     <!--<div  style="float:right;">
-      <GmapMap
-      :center="{lat:42, lng:-8}"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-      >
-      <GmapMarker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-      />
-    </GmapMap>
-  </div>-->
+        <GmapMap
+        :center="{lat:42, lng:-8}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+        >
+        <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+        />
+      </GmapMap>
+    </div>-->
 
-</div>
+   
+  </div>
 </template>
 <script>
 
@@ -94,12 +114,14 @@ export default {
       })
       .catch(e => {
         this.errors.push(e)
-      })
+      });
     },
 
-    updateCounter(id){
-      var url = 'http://localhost:3000/users/updateCounter/'+id;
-      axios.patch(url)
+    updateCounter(users){
+      users.forEach(element => {
+        var url = 'http://localhost:3000/users/updateCounter/'+element.id;
+        axios.patch(url)
+      });
     },
 
 
