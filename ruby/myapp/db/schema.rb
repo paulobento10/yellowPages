dc75d08c1905bf8cb3d11e032cff3c4dfee8df9d
Paulo <paulobento1997@gmail.com>
2019-11-17 22:41:17 +0000

    Trying to add image through paperclip

--- ruby/myapp/db/schema.rb
+++ ruby/myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_181602) do
+ActiveRecord::Schema.define(version: 2019_11_17_192417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_11_03_181602) do
     t.datetime "updated_at", null: false
     t.string "link"
     t.integer "counter"
+    t.string "image"
+    t.string "avatar"
   end
 
 end
